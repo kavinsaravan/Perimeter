ef4be44a2bf6093dd54d2b38d589b59106b485be
kavins <kavinsaravan@gmail.com>
2020-06-17 06:28:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var length = window.prompt("Enter length of a rectangle:");
+var width = window.prompt("Enter width of a rectangle:");
+var sum = parseInt(length) + parseInt(width)
+alert("The perimeter is" + 2 * (sum));
